cf3921e1f99cdb00648fe794998781cbd2bdfb15
Nick Eurek <neurek88@gmail.com>
2015-12-08 17:36:50 -0600

    Revert "able to load new data"
    This reverts commit bb5ed2e792c29701ef680096d3d18024c96ef2c0.

--- nvChart.js
+++ nvChart.js
@@ -1,6 +1,6 @@
+$(document).ready(function() {
+
   var csvData;
-var myData;
-var chart;
                   d3.csv("data/bengals_history.csv", function(error, data) {
                     data.forEach(function(d) {
                       d.BengalsWins = +d.Bengals_Wins;
@@ -34,8 +34,9 @@ console.log(wins);
     },
   ];
 }
+
   nv.addGraph(function() {
-  chart = nv.models.lineChart()
+  var chart = nv.models.lineChart()
                 .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
                 .useInteractiveGuideline(false)  //We want nice looking tooltips and a guideline!
                 .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
@@ -50,7 +51,7 @@ console.log(wins);
       .axisLabel('Wins')
 
   // Done setting the chart up? Time to render it!
- myData = nvData();
+  var myData = nvData();
    console.log(myData);//You need data...
 
   d3.select('svg')    //Select the <svg> element you want to render the chart in.
@@ -64,30 +65,5 @@ console.log(wins);
 });
 })
 
-    function loadData() {
-d3.csv("data/bearsHistory.csv", function(error, data) {
-                    data.forEach(function(d) {
-                      d.BearsWins = +d.Bears_Wins;
-                      d.year = +d.Year;
-                      });
-              console.log(data);
-  var wins = []
 
-  //Data is represented as an array of {x,y} pairs.
-  for (var i = 0; i < data.length; i++) {
-    wins.push({x: data[i].year, y: data[i].BearsWins});
-  }
-console.log(wins);
-  //Line chart data should be sent as an array of series objects.
-  myData.push(
-    {
-      values: wins,      //values - represents the array of {x,y} data points
-      key: 'Bears Wins', //key  - the name of the series.
-      color: '#000999'  //color - optional: choose your own line color.
-    }
-    )
-  console.log(myData);
-  chart.update()
 })
-                  }
-
