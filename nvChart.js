e8d597601ea7902643c3423d12407ad7c82c6c39
Nick Eurek <neurek88@gmail.com>
2015-12-08 15:33:39 -0600

    Added nvd3 chart with working tooltip

--- nvChart.js
+++ nvChart.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-var csvData;
+
+  var csvData;
                   d3.csv("data/bengals_history.csv", function(error, data) {
                     data.forEach(function(d) {
                       d.BengalsWins = +d.Bengals_Wins;
@@ -8,47 +9,17 @@ var csvData;
                       d.Dpts = (33- (+d.DefenseRank));
                   });
               csvData = data;
-                console.log(csvData);
-
-  nv.addGraph(function() {
-  var chart = nv.models.lineChart()
-                .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
-                .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
-                .transitionDuration(350)  //how fast do you want the lines to transition?
-                .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
-                .showYAxis(true)        //Show the y-axis
-                .showXAxis(true)        //Show the x-axis
-  ;
-
-  chart.xAxis     //Chart x-axis settings
-      .axisLabel('Year')
-
-  chart.yAxis     //Chart y-axis settings
-      .axisLabel('Wins')
-
-  /* Done setting the chart up? Time to render it!*/
-  var myData = nvData();
-   //You need data...
-
-  d3.select('#graph svg')    //Select the <svg> element you want to render the chart in.
-      .datum(myData)         //Populate the <svg> element with chart data...
-      .call(chart);          //Finally, render the chart!
-
-  //Update the chart when window resizes.
-  nv.utils.windowResize(function() { chart.update() });
-  return chart;
-});
-})
-
-  function nvData () {
-var wins = [], OffRank = [];
+                console.log(csvData)
+function nvData () {
+  var wins = [], OffRank = [];
 
   //Data is represented as an array of {x,y} pairs.
   for (var i = 0; i < csvData.length; i++) {
     wins.push({x: csvData[i].year, y: csvData[i].BengalsWins});
     OffRank.push({x: csvData[i].year, y: csvData[i].Opts});
   }
-
+console.log(wins);
+  console.log(OffRank);
   //Line chart data should be sent as an array of series objects.
   return [
     {
@@ -62,6 +33,37 @@ var wins = [], OffRank = [];
       color: '#2ca02c'
     },
   ];
-    console.log(wins);
 }
+
+  nv.addGraph(function() {
+  var chart = nv.models.lineChart()
+                .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
+                .useInteractiveGuideline(false)  //We want nice looking tooltips and a guideline!
+                .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
+                .showYAxis(true)        //Show the y-axis
+                .showXAxis(true)        //Show the x-axis
+  ;
+
+  chart.xAxis     //Chart x-axis settings
+      .axisLabel('Year')
+
+  chart.yAxis     //Chart y-axis settings
+      .axisLabel('Wins')
+
+  // Done setting the chart up? Time to render it!
+  var myData = nvData();
+   console.log(myData);//You need data...
+
+  d3.select('svg')    //Select the <svg> element you want to render the chart in.
+      .datum(myData)         //Populate the <svg> element with chart data...
+      .transition().duration(500)
+      .call(chart);          //Finally, render the chart!
+
+  //Update the chart when window resizes.
+  nv.utils.windowResize(function() { chart.update() });
+  return chart;
+});
+})
+
+
 })
